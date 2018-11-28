abfd86289a7c8fc11a336c50cabfc8ffc94884db
Dan <dendybox@yandex.ru>
2018-11-28 21:38:40 +0700

    Добавляет функцию генерацию комментариев и описания

--- js/pictures.js
+++ js/pictures.js
@@ -10,14 +10,15 @@ var COMMENTS_LIST = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
 var DESCRIPTIONS_LIST = [
   'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
   'Отдыхаем...',
   'Цените каждое мгновенье. Цените тех, кто рядом и с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'
-];
+  'Вот это тачка!'];
+
 var LIKES_MIN = 15;
 var LIKES_MAX = 200;
 var pictureList = [];
@@ -33,13 +34,31 @@ var randomCount = function (max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var generatePicture = function (list, pictureAddress, number, extension, likesMax, likesMin) {
+var getCommentsList = function (commentsList) {
+  var pictureComments = [];
+  pictureComments.length = randomCount(1, commentsList.length);
+  for (var i = 0; i < pictureComments.length; i++) {
+    pictureComments[i] = commentsList[randomCount(commentsList.length)];
+  }
+  return pictureComments;
+};
+
+var getDescription = function (descriptionsList) {
+  return descriptionsList[randomCount(descriptionsList.length)];
+};
+
+var generatePicture = function (list, pictureAddress, number, extension, likesMax, likesMin, commentsList, descriptionsList) {
   var picture = {
     url: getPictureUrl(pictureAddress, number, extension),
-
+    likes: randomCount(likesMax, likesMin),
+    comments: getCommentsList(commentsList),
+    description: getDescription(descriptionsList)
   };
 
   return picture;
 };
 
-generatePicture(pictureList, PICTURE_ADDRESS, 5, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN);
+for (var i = 0; i < PICTURE_AMOUNT; i++) {
+  console.log(generatePicture(pictureList, PICTURE_ADDRESS, i, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
+}
+

0744cff39212ac5ffef3b8f53603858d51fef3cb
Dan <dendybox@yandex.ru>
2018-11-29 00:28:14 +0700

    Добавляет отрисовку блока увеличенного изображения с информацией о нем

--- js/pictures.js
+++ js/pictures.js
@@ -28,6 +28,11 @@ var picture = document.querySelector('#picture')
     .content
     .querySelector('.picture');
 
+var bigPicture = document.querySelector('.big-picture');
+var BIG_PICTURE_EXAMPLE = 1;
+var commentCount = document.querySelector('.social__comment-count');
+var commentsLoader = document.querySelector('.comments-loader');
+
 var getPictureUrl = function (address, number, extension) {
   return address + number + extension;
 };
@@ -68,16 +73,33 @@ var createPictureElement = function (pictureElement, pictureBlock) {
   pictureBlock.querySelector('.picture__comments').textContent = pictureElement.comments.length;
 };
 
-var renderPicture = function (i, pictureElementList, pictureAddress, pictureExtension, likeMin, likeMax, commentsList, descriptionsList) {
+var renderPictureElement = function (i, pictureElementList, pictureAddress, pictureExtension, likeMin, likeMax, commentsList, descriptionsList) {
   picturesElementsList[i] = generatePicture(picturesElementsList, pictureAddress, i, pictureExtension, likeMin, likeMax, commentsList, descriptionsList);
   var pictureBlock = picture.cloneNode(true);
   createPictureElement(picturesElementsList[i], pictureBlock);
 
   return pictureBlock;
 };
-for (var i = 0; i < PICTURE_AMOUNT; i++) {
-  fragment.appendChild(renderPicture(i, picturesElementsList, PICTURE_ADDRESS, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
-}
-pictures.appendChild(fragment);
+
+var renderPictures = function () {
+  for (var i = 0; i < PICTURE_AMOUNT; i++) {
+    fragment.appendChild(renderPictureElement(i, picturesElementsList, PICTURE_ADDRESS, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
+  }
+  pictures.appendChild(fragment);
+};
+
+var renderBigPicture = function () {
+  bigPicture.classList.remove('hidden');
+
+  bigPicture.querySelector('.big-picture__img').src = picturesElementsList[BIG_PICTURE_EXAMPLE].url;
+  bigPicture.querySelector('.likes-count').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].likes;
+  bigPicture.querySelector('.comments-count').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].comments.length;
+  bigPicture.querySelector('.social__caption').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].description;
 
 
+  commentCount.classList.add('visually-hidden');
+  commentsLoader.classList.add('visually-hidden');
+};
+
+renderPictures();
+renderBigPicture();

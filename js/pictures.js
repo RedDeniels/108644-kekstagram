24b7127e0e424ccc1a6ad87d89d47b64bb696d2a
Dan <dendybox@yandex.ru>
2018-11-28 23:41:42 +0700

    Добавляет отрисовку изображений в блок .pictures

--- js/pictures.js
+++ js/pictures.js
@@ -21,7 +21,12 @@ var DESCRIPTIONS_LIST = [
 
 var LIKES_MIN = 15;
 var LIKES_MAX = 200;
-var pictureList = [];
+var picturesElementsList = [];
+var fragment = document.createDocumentFragment();
+var pictures = document.querySelector('.pictures');
+var picture = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
 
 var getPictureUrl = function (address, number, extension) {
   return address + number + extension;
@@ -48,17 +53,31 @@ var getDescription = function (descriptionsList) {
 };
 
 var generatePicture = function (list, pictureAddress, number, extension, likesMax, likesMin, commentsList, descriptionsList) {
-  var picture = {
-    url: getPictureUrl(pictureAddress, number, extension),
+  var pictureInfo = {
+    url: getPictureUrl(pictureAddress, number + 1, extension),
     likes: randomCount(likesMax, likesMin),
     comments: getCommentsList(commentsList),
     description: getDescription(descriptionsList)
   };
-
-  return picture;
+  return pictureInfo;
 };
 
+var createPictureElement = function (pictureElement, pictureBlock) {
+  pictureBlock.querySelector('.picture__img').src = pictureElement.url;
+  pictureBlock.querySelector('.picture__likes').textContent = pictureElement.likes;
+  pictureBlock.querySelector('.picture__comments').textContent = pictureElement.comments.length;
+};
+
+var renderPicture = function (i, pictureElementList, pictureAddress, pictureExtension, likeMin, likeMax, commentsList, descriptionsList) {
+  picturesElementsList[i] = generatePicture(picturesElementsList, pictureAddress, i, pictureExtension, likeMin, likeMax, commentsList, descriptionsList);
+  var pictureBlock = picture.cloneNode(true);
+  createPictureElement(picturesElementsList[i], pictureBlock);
+
+  return pictureBlock;
+};
 for (var i = 0; i < PICTURE_AMOUNT; i++) {
-  console.log(generatePicture(pictureList, PICTURE_ADDRESS, i, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
+  fragment.appendChild(renderPicture(i, picturesElementsList, PICTURE_ADDRESS, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
 }
+pictures.appendChild(fragment);
+
 

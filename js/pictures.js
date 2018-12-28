1e7f62d539d310403cab5d06d7a31d3e9b8c6b0e
RedDeniels <dendybox@yandex.ru>
2018-12-28 22:27:58 +0700

    Добавляет модуль picture.js отрисовывающий миниатюры изображений

--- js/pictures.js
+++ js/pictures.js
@@ -1,67 +1,15 @@
 'use strict';
 
-var PICTURE_ADDRESS = 'photos/';
-var PICTURE_EXTENSION = '.jpg';
-var COMMENTS_LIST = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо было бы убирать палец из кадра.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-var DESCRIPTIONS_LIST = [
-  'Тестим новую камеру!',
-  'Затусили с друзьями на море',
-  'Как же круто тут кормят',
-  'Отдыхаем...',
-  'Цените каждое мгновенье. Цените тех, кто рядом и с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'];
-
-
-var LIKES_MIN = 15;
-var LIKES_MAX = 200;
-var picturesElementsList = [];
-
 var COMMENT_AVATAR_ADDRESS = 'img/';
 var COMMENT_AVATAR_PREFIX = 'avatar-';
 var COMMENT_AVATAR_EXTENSION = '.svg';
 var COMMENT_AMOUNT = 6;
-
-var PICTURE_AMOUNT = 25;
-
-var pictures = document.querySelector('.pictures');
-var picture = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
 
 var bigPicture = document.querySelector('.big-picture');
 var BIG_PICTURE_EXAMPLE = 1;
 var commentCount = document.querySelector('.social__comment-count');
 var commentsLoader = document.querySelector('.comments-loader');
 var socialComments = document.querySelector('.social__comments');
-
-var createPictureElement = function (pictureElement, pictureBlock) {
-  pictureBlock.querySelector('.picture__img').src = pictureElement.url;
-  pictureBlock.querySelector('.picture__likes').textContent = pictureElement.likes;
-  pictureBlock.querySelector('.picture__comments').textContent = pictureElement.comments.length;
-};
-
-var renderPictureElement = function (i, pictureElementList, pictureAddress, pictureExtension, likeMin, likeMax, commentsList, descriptionsList) {
-  picturesElementsList[i] = window.data.generatePicture(picturesElementsList, pictureAddress, i, pictureExtension, likeMin, likeMax, commentsList, descriptionsList);
-  var pictureBlock = picture.cloneNode(true);
-  createPictureElement(picturesElementsList[i], pictureBlock);
-
-  return pictureBlock;
-};
-
-var renderPictures = function () {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < PICTURE_AMOUNT; i++) {
-    fragment.appendChild(renderPictureElement(i, picturesElementsList, PICTURE_ADDRESS, PICTURE_EXTENSION, LIKES_MAX, LIKES_MIN, COMMENTS_LIST, DESCRIPTIONS_LIST));
-  }
-  pictures.appendChild(fragment);
-};
 
 var hidingDefaultComments = function () {
   var socialCommentsList = document.querySelectorAll('.social__comment');
@@ -94,16 +42,12 @@ var renderBigPicture = function () {
   commentsLoader.classList.add('visually-hidden');
   hidingDefaultComments();
 
-  bigPicture.querySelector('.big-picture__img img').src = picturesElementsList[BIG_PICTURE_EXAMPLE].url;
-  bigPicture.querySelector('.likes-count').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].likes;
-  bigPicture.querySelector('.comments-count').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].comments.length;
-  bigPicture.querySelector('.social__caption').textContent = picturesElementsList[BIG_PICTURE_EXAMPLE].description;
-  renderBigPictureCommentsList(picturesElementsList[BIG_PICTURE_EXAMPLE].comments, socialComments);
+  bigPicture.querySelector('.big-picture__img img').src = window.picture.picturesElementsList[BIG_PICTURE_EXAMPLE].url;
+  bigPicture.querySelector('.likes-count').textContent = window.picture.picturesElementsList[BIG_PICTURE_EXAMPLE].likes;
+  bigPicture.querySelector('.comments-count').textContent = window.picture.picturesElementsList[BIG_PICTURE_EXAMPLE].comments.length;
+  bigPicture.querySelector('.social__caption').textContent = window.picture.picturesElementsList[BIG_PICTURE_EXAMPLE].description;
+  renderBigPictureCommentsList(window.picture.picturesElementsList[BIG_PICTURE_EXAMPLE].comments, socialComments);
 };
-
-renderPictures();
-
-// ---- 4 задание ----
 
 var bigPictureCancel = document.querySelector('.big-picture__cancel');
 var smallPictures = document.querySelectorAll('.picture');
